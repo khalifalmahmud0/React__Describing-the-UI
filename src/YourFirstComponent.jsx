a90cb22196d15c71c0c2a16fd41becadb899d919
khalifalmahmud0 <code.khalif@gmail.com>
2023-06-08 12:03:49 +0600

    YourFirstComponent

---
+++ src/YourFirstComponent.jsx
@@ -0,0 +1,17 @@
+import Profile from "./YourFirstComponent/Profile";
+
+export default function YourFirstComponent() {
+	return (
+		<>
+			<h1>Practicing First Component</h1>
+			<span>
+				a React component is a JavaScript function that you can sprinkle with
+				markup.
+			</span>
+			<br />
+			<Profile />
+			<Profile />
+			<Profile />
+		</>
+	);
+}
---
+++ src/YourFirstComponent/Profile.jsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+	return (
+		<>
+			<img
+				src="https://thumbs.dreamstime.com/b/vector-illustration-avatar-dummy-logo-collection-image-icon-stock-isolated-object-set-symbol-web-137160339.jpg"
+				alt="Katherine Johnson"
+				height="50px"
+			/>
+		</>
+	);
+}
